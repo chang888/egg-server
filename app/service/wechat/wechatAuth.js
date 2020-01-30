b271d91acceff6605fed6a2ae508aec7ceaf595b
zheng <6291@163.com>
2020-01-30 20:37:59 +0800

    修改微信服务

--- app/service/wechat/wechatAuth.js
+++ app/service/wechat/wechatAuth.js
@@ -3,7 +3,9 @@
 const { Service } = require("egg")
 
 class WechatAuthService extends Service {
-  async setToken() {}
+  async setToken() {
+    const { ctx, app } = this
+  }
 }
 
 module.exports = WechatAuthService
