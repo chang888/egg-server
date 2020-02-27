33445c50170fa080bb5eacd9150ae6cc89ad6223
zheng <6291@163.com>
2020-02-27 17:14:30 +0800

    增加webhooks

---
+++ webhooks.js
@@ -0,0 +1,49 @@
+const http = require("http")
+const createHandler = require("github-webhook-handler")
+const handler = createHandler({
+  path: "/docker_deploy",
+  secret: "myHashSecret"
+})
+const { spawn } = require("child_process")
+function run_cmd(cmd, args, callback) {
+  const child = spawn(cmd, args)
+  let resp = ""
+  child.stdout.on("data", function(buffer) {
+    resp += buffer.toString()
+  })
+  child.stdout.on("end", function() {
+    callback(resp)
+  })
+}
+
+http
+  .createServer((req, res) => {
+    handler(req, res, err => {
+      console.log("收到请求githooks", req)
+
+      //   res.statusCode = 200
+      res.statusCode = 404
+      res.end("no such location")
+    })
+  })
+  .listen(7777, () => {
+    console.log("Webhook listen at 7777")
+  })
+
+handler.on("error", err => {
+  console.error("Error", err.message)
+})
+
+handler.on("push", event => {
+  console.log(event, "收到event")
+
+  if (event.payload.ref === "refs/heads/init") {
+    console.log("Receive push ")
+    console.log("收到修改的文件", event.payload.head_commit)
+    console.log("===========", event.payload.head_commit.modified)
+
+    run_cmd("sh", ["./deploy-dev.sh"], function(text) {
+      console.log(text)
+    })
+  }
+})
